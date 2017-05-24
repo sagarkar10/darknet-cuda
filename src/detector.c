a242c1cec9e84beddfb41d7c5771c1ecf0fbc38b
sagar kar <sagarkar10@gmail.com>
2017-05-24 17:22:18 +0530

    updated local

--- src/detector.c
+++ src/detector.c
@@ -561,7 +561,7 @@ void validate_detector_recall(char *cfgfile, char *weightfile)
     fprintf(stderr, "Learning Rate: %g, Momentum: %g, Decay: %g\n", net.learning_rate, net.momentum, net.decay);
     srand(time(0));
 
-    list *plist = get_paths("data/voc.2007.test");
+    list *plist = get_paths("2007_test.txt");
     char **paths = (char **)list_to_array(plist);
 
     layer l = net.layers[net.n-1];
