8910988e4b39e50754a1f4a07d5ccb9b64634a46
sagarkar10 <sagarkar10@gmail.com>
2017-05-24 17:55:10 +0530

    from beast

--- scripts/voc_label.py
+++ scripts/voc_label.py
@@ -23,8 +23,8 @@ def convert(size, box):
     return (x,y,w,h)
 
 def convert_annotation(year, image_id):
-    in_file = open('miniVOCdevkit/VOC%s/Annotations/%s.xml'%(year, image_id))
-    out_file = open('miniVOCdevkit/VOC%s/labels/%s.txt'%(year, image_id), 'w')
+    in_file = open('VOCdevkit/VOC%s/Annotations/%s.xml'%(year, image_id))
+    out_file = open('VOCdevkit/VOC%s/labels/%s.txt'%(year, image_id), 'w')
     tree=ET.parse(in_file)
     root = tree.getroot()
     size = root.find('size')
@@ -45,12 +45,12 @@ def convert_annotation(year, image_id):
 wd = getcwd()
 
 for year, image_set in sets:
-    if not os.path.exists('miniVOCdevkit/VOC%s/labels/'%(year)):
-        os.makedirs('miniVOCdevkit/VOC%s/labels/'%(year))
-    image_ids = open('miniVOCdevkit/VOC%s/ImageSets/Main/%s.txt'%(year, image_set)).read().strip().split()
+    if not os.path.exists('VOCdevkit/VOC%s/labels/'%(year)):
+        os.makedirs('VOCdevkit/VOC%s/labels/'%(year))
+    image_ids = open('VOCdevkit/VOC%s/ImageSets/Main/%s.txt'%(year, image_set)).read().strip().split()
     list_file = open('%s_%s.txt'%(year, image_set), 'w')
     for image_id in image_ids:
-        list_file.write('%s/miniVOCdevkit/VOC%s/JPEGImages/%s.jpg\n'%(wd, year, image_id))
+        list_file.write('%s/VOCdevkit/VOC%s/JPEGImages/%s.jpg\n'%(wd, year, image_id))
         convert_annotation(year, image_id)
     list_file.close()
 
